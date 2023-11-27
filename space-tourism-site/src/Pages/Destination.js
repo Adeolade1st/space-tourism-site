05774c3e3b14a132ae48d23b54b9f4f87721246d
Ilavbare Adeola <raisingsrs@gmail.com>
2023-11-27 22:31:44 +0100

    Destination Moon Update

--- space-tourism-site/src/Pages/Destination.js
+++ space-tourism-site/src/Pages/Destination.js
@@ -1,6 +1,7 @@
 import React from "react";
 import mainLogo from './Path.png';
 import horizontal from './Rectangle.png';
+import hr from './hr-line.png';
 import bgDestination from './background-destination-desktop.jpg';
 import moon from './image-moon.png';
 import "./destination.css";
@@ -26,12 +27,12 @@ export default function Destination(){
                     <div className="container">
                         <p className="moon-head">01 PICK YOUR DESTINATION</p>
                         <div className="row">
-                            <div className="col-sm-8"><img src={moon} alt="moon" className="moon"/></div>
+                            <div className="col-sm-7"><img src={moon} alt="moon" className="moon"/></div>
                             <div className="col-sm-4"> <span ><button className="btn">MOON</button></span> 
                              <span ><button className="btn">MARS</button></span> <span ><button className="btn ">EUROPA</button>
                              </span> <span ><button className="btn ">TITAN</button></span> </div>
                              <p className="mon-dest">MOON</p>
-                             <div className="col">  <p className="see">See our planet as you’ve never seen it before. A perfect relaxing trip 
+                             <div className="col-5">  <p className="see">See our planet as you’ve never seen it before. A perfect relaxing trip 
                                 away to help regain perspective and come back refreshed. 
                                 While you’re there, take in some history by visiting the 
                                 Luna 2 and Apollo 11 landing sites.</p>
@@ -39,15 +40,21 @@ export default function Destination(){
                                  
                                
                              </div>
-                              <div> <img src={horizontal} className="horizontal-body" alt="horizontal-line"></img></div>
+                            
                               
                               <div className="row">
-                              
-                                <div className="col-6">AVG. DISTANCE</div>
-                                 <div className="col-6">Est. travel time</div>
-                                 <div className="col-6">384,400 km</div>
-                                 <div className="col-6">3 days</div>
-                                    </div>
+                                <div> < img src={hr} className="hr-body" alt="horizontal-line"></img></div>
+                                <div className="col-6 avg">AVG. DISTANCE</div>
+                                 <div className="col-6 est">EST. TRAVEL TIME</div>
+                                  <div className="col-6 km">384,400 KM</div>
+                                     <div className="col-6 days">3 DAYS</div>
+                                  </div>
+                            
+                             
+                                  
+                                 
+                                
+                                   
                              
                                 
                            
