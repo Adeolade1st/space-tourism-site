924513c65310d0014c3f8abdb2458de9a9ee5fee
Ilavbare Adeola <raisingsrs@gmail.com>
2023-12-08 03:08:20 +0100

    Responsive Home Page

--- space-tourism-site/src/Pages/HomePage.js
+++ space-tourism-site/src/Pages/HomePage.js
@@ -10,6 +10,7 @@ import './homepage.css';
 
 
 
+
 export default function HomePage(){
 
 
@@ -19,32 +20,30 @@ export default function HomePage(){
     return(
 
         <div>
-            <img src= {desktopBackground} alt="desktop-background" className="desktopHome img-fluid "  ></img>
+            <img src= {desktopBackground} alt="desktop-background" className="desktopHome img-fluid max-width: 100% height:auto"  ></img>
              <img src= {mobileBackground} alt="mobile-background" className="mobileHome img-fluid max-width: 100% height:auto " ></img>
-                            <span> <img src={mainLogo} className="logo-top" alt="logo"></img></span>
-                            <span> <img src={horizontal} className="horizontal-line" alt="horizontal-line"></img></span>
+                           
          
          
     <div className="container">
+     <span> <img src={mainLogo} className="logo-top" alt="logo"></img></span>
+        <span> <img src={horizontal} className="horizontal-line" alt="horizontal-line"></img></span>
         <div className="container hero"> 
            <p className="travel">SO, YOU WANT TO TRAVEL TO  </p>
-                    <h1 className="space-on">SPACE</h1>
-
            <div className="row">
             <div className="col-sm-5">
-              <p className="faceit-now ">
-                         
-                            Let’s face it; if you want to go to space, you might as well genuinely go to outer 
-                            space and not hover kind of on the edge of it.
-                            Well sit back, and relax because we’ll give you a truly out 
-                            of this world experience! Explore.
+             <h1 className="space-on">SPACE</h1>
+              <p className="faceit-now">
+                           Let’s face it; if you want to go to space, you might as well genuinely go to 
+                           outer space and not hover kind of on the edge of it. 
+                           Well sit back, and relax because we’ll give you a truly out of this world experience!
                         
               </p>
               
             </div>
             <div className="col-sm-7">
-                      <button className="btn rounded-circle in"><div className="explore-more">EXPLORE</div> </button>
-
+                      <button className="btn btn rounded-circle in"><div className="explore-more">EXPLORE</div> </button>
+                      <button className="btn btn rounded">EXPLORE</button>
                    </div>
 
            </div>
