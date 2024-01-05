4f48dc69034100cd97ec2066dd8f7c17f0cb45b4
Ilavbare Adeola <raisingsrs@gmail.com>
2024-01-05 18:58:21 +0100

    Latest Update

--- space-tourism-site/src/App.js
+++ space-tourism-site/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Destination from "./Pages/Destination";
 import Crew from "./Pages/Crew";
@@ -10,12 +10,20 @@ import {BrowserRouter as Router,Routes,Route, Link, BrowserRouter} from "react-r
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
 
 
-function App() {
-  
+class App extends Component{
+
+  state={clicked:false};
+  handleclick = () =>{
+    this.setState({clicked:! this.state.clicked})
+  }
+  render(){
+
+ 
   return (    
     <BrowserRouter>
         
       <nav >
+        
         <div className="logo"><img src={mainLogo}  alt="logo"></img></div>
         <div className="horizonal-line"><img src={horizontal}  alt="horizontal-line"></img></div>
           <Link to='/' className="active"><strong>00</strong> HOME</Link>
@@ -23,9 +31,12 @@ function App() {
             <Link to='/Crew'><strong>02</strong> CREW</Link>
              <Link to='/Technology'><strong>03</strong> TECHNOLOGY</Link>
             
-             <div id="mobile-view" className="fas fa-bars"></div>
-             <div id="times"className="fas fa-times"></div>
+            <div id="mobile-view" onClick={this.handleclick}>
+              
+             <div id="times"  className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></div>
+            </div>
 
+        
      </nav>
 
      <Routes>
@@ -41,5 +52,7 @@ function App() {
   );
   
 }
+ }
+  
 
 export default App;
